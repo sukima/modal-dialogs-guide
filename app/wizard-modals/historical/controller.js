7b62f4393e8d1a52d8aec73a0ebea8359a2b549f
Devin Weaver <suki@tritarget.org>
2019-12-12 10:20:16 -0500

    WIP  [skip ci]

--- app/wizard-modals/historical/controller.js
+++ app/wizard-modals/historical/controller.js
@@ -1,11 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { run } from '@ember/runloop';
 
 export default class WizardModalsHistoricalController extends Controller {
 
   @action openModal(resetHistory) {
     resetHistory();
-    this.modalManager.open();
+    // Glimmer needs a nano-moment to update @currentState
+    run.next(() => this.modalManager.open());
   }
 
 }
