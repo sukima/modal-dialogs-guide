39ea1772476c640bc2d83d8f6261915e0433fbb5
Devin Weaver <suki@tritarget.org>
2019-12-14 00:07:30 -0500

    WIP  [skip ci]

--- app/components/goldilocks-story-modal/cover/component.js
+++ app/components/goldilocks-story-modal/cover/component.js
@@ -1,8 +1,11 @@
 import Component from '@ember/component';
+import config from 'modal-dialogs-guide/config/environment';
 
 export default class StoryCoverComponent extends Component {
 
   tagName = '';
+
+  rootURL = config.rootURL;
 
   didInsertElement() {
     super.didInsertElement(...arguments);
